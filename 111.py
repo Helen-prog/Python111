db221f5a9950e97978b0424b6c3086d5e7ff258a
Helen <elena.kozovyakina@gmail.com>
2021-12-19 10:06:51 +0300

    second commit

--- 111.py
+++ 111.py
@@ -3199,14 +3199,14 @@ import time
 # print(os.path.isfile(r"D:\pythonProject5\text\three.txt"))
 # print(os.path.isdir(r"D:\pythonProject5\text"))
 
-dir_name = "Work"
-
-objs = os.listdir(dir_name)
-print(objs)
-for obj in objs:
-    p = os.path.join(dir_name, obj)
-    # print(p)
-    if os.path.isfile(p):
-        print(f"{obj} - file - {os.path.getsize(p)} bytes")
-    elif os.path.isdir(p):
-        print(f"{obj} - dir")
+# dir_name = "Work"
+#
+# objs = os.listdir(dir_name)
+# print(objs)
+# for obj in objs:
+#     p = os.path.join(dir_name, obj)
+#     # print(p)
+#     if os.path.isfile(p):
+#         print(f"{obj} - file - {os.path.getsize(p)} bytes")
+#     elif os.path.isdir(p):
+#         print(f"{obj} - dir")
